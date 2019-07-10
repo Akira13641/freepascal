f57ed34119a2946dff9091e06f92cf5a991f8206
BenGrasset <operator97@gmail.com>
2019-07-10 00:02:47 -0400

    Add tests for 'public' and 'external' with multi-line strings

---
+++ tests/test/tmultilinestring20.pp
@@ -0,0 +1,10 @@
+program tmultilinestring20;
+
+{$modeswitch MultiLineStrings}
+{$MultiLineStringTrimLeft 2}
+
+uses tmultilinestringunit2;
+
+begin
+  DoIt;
+end.
---
+++ tests/test/tmultilinestringunit2.pp
@@ -0,0 +1,17 @@
+unit tmultilinestringunit2;
+
+{$modeswitch MultiLineStrings}
+{$MultiLineStringTrimLeft 2}
+
+interface
+
+procedure DoIt;
+
+implementation
+
+procedure DoIt; public name `SingleLineMultiLine`;
+begin
+  Write('Ok!');
+end;
+
+end.
