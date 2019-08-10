d82224104572599fca5811fc32d1d9da7a80de0e
BenGrasset <operator97@gmail.com>
2019-08-10 13:22:07 -0400

    Adjust some comments in test files.

--- tests/test/tmultilinestring22.pp
+++ tests/test/tmultilinestring22.pp
@@ -3,6 +3,8 @@ program tmultilinestring22;
 {$modeswitch MultiLineStrings}
 {$MultiLineStringTrimLeft 5}
 
+{ ↓↓↓ the point here being that it no longer disappears }
+
 const Test = ` ThisSpace>>>>     <<<<disappeared`;
 
 begin
